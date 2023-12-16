c7181e46bb234bc686bdeef661892c977647bad8
Satarupa Deb <84276219+Satarupa22-SD@users.noreply.github.com>
2023-12-16 20:22:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain.llms import CTransformers
 ## Function To get response from LLAma 2 model
 
 def getLLamaresponse(input_text, no_words, blog_style):
-    ### LLama2 model
+    # LLama2 model
     llm = CTransformers(model='models/llama-2-7b-chat.ggmlv3.q8_0.bin',
                         model_type='llama',
                         config={'max_new_tokens': 256,
@@ -22,30 +22,30 @@ def getLLamaresponse(input_text, no_words, blog_style):
     prompt = PromptTemplate(input_variables=["blog_style", "input_text", 'no_words'],
                             template=template)
 
-    ## Generate the ressponse from the LLama 2 model
+    ## Generating the ressponse from the LLama 2 model
     response = llm(prompt.format(blog_style=blog_style, input_text=input_text, no_words=no_words))
     print(response)
     return response
 
 
 st.set_page_config(page_title="Generate Blogs",
-                   page_icon='🤖',
+                   page_icon='🗒️',
                    layout='centered',
                    initial_sidebar_state='collapsed')
 
-st.header("Generate Blogs 🤖")
+st.header("Generate Blogs 🗒️")
 
-input_text = st.text_input("Enter the Blog Topic")
+input_text = st.text_input("Enter the Blog Topic:")
 
 ## creating to more columns for additonal 2 fields
 
 col1, col2 = st.columns([5, 5])
 
 with col1:
-    no_words = st.text_input('No of Words')
+    no_words = st.text_input('Number of Words:')
 with col2:
-    blog_style = st.selectbox('Writing the blog for',
-                              ('Researchers', 'Data Scientist', 'Common People'), index=0)
+    blog_style = st.selectbox('Writing the blog for:',
+                              ('Researchers', 'Data Scientist', 'Engineers', 'Students'), index=0)
 
 submit = st.button("Generate")
 
